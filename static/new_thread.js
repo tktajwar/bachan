ace27e4b577e96b3a37daa83e914bdce12e9ba50
Takey Tajwar <tajwar.earth@pm.me>
2026-02-06 13:46:57 +0600

    remove image input from thread form

--- static/new_thread.js
+++ static/new_thread.js
@@ -3,10 +3,3 @@ document.getElementById("newThreadButton").onclick = function(event) {
     document.getElementById("postFormContainer").style.display = "flex";
     document.getElementById("newThreadButton").style.display = "none";
 };
-
-document.getElementById("image-upload").onchange = function() {
-    if(this.files[0].size > 2097152) {
-       alert("File is too big!");
-       this.value = "";
-    }
-};
