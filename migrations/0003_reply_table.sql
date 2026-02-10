a3697d887e1107d1c0ab5bbef19056e8769d6df4
Takey Tajwar <tajwar.earth@pm.me>
2026-02-10 12:09:54 +0600

    make reply's tid referene thread's id

--- migrations/0003_reply_table.sql
+++ migrations/0003_reply_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE reply (
        id INT DEFAULT nextval('id') PRIMARY KEY,
        uid INT NOT NULL,
-       tid INT NOT NULL,
+       tid INT NOT NULL REFERENCES thread(id),
        comment TEXT NOT NULL,
        ctime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
