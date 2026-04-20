1eeaa3a07575d925a1afca386fb377de54e6c8c1
Takey Tajwar <tajwar.earth@pm.me>
2026-04-20 20:03:11 +0600

    update url of boards to 4chan standards

---
+++ migrations/2016_update_boards_to_4chan_style.sql
@@ -0,0 +1,15 @@
+INSERT INTO board (url, label, category)
+VALUES
+    ('mu', 'মিউজিক', 'Hobbies'),
+    ('sci', 'বিজ্ঞান ও গণিত', 'Interests'),
+    ('his', 'মানবতা ও ইতিহাস', 'Interests'),
+    ('lit', 'সাহিত্য', 'Interests')
+ON CONFLICT (url)
+DO NOTHING;
+
+UPDATE thread SET board = 'mu' WHERE board = 's';
+UPDATE thread SET board = 'sci' WHERE board = 'bg';
+UPDATE thread SET board = 'his' WHERE board = 'me';
+UPDATE thread SET board = 'lit' WHERE board = 'sh';
+
+DELETE FROM board WHERE url IN ('s', 'bg', 'me', 'sh');
