416f6f3dc99387da8d3af6ae7e2d3fc5261b604c
Takey Tajwar <tajwar.earth@pm.me>
2026-04-28 19:40:44 +0600

    Revert the number of top threads back to 24
    This reverts commit 574c80d8c67a1f07eb6a57be435ca72ad05e8266.

--- src/pages/root_handler.rs
+++ src/pages/root_handler.rs
@@ -40,7 +40,7 @@ pub async fn root_page(
     let mut ctx = tera::Context::new();
 
     let (threads, last_mtime) = match top_threads (
-	36,
+	24,
 	pool_state.clone(),
     ).await {
 	Ok(result) => result,
