1e72f61f88cbb133806b7925ef18c05503a77489
Takey Tajwar <tajwar.earth@pm.me>
2026-07-25 12:43:38 +0600

    replace ref thread button with mtime delta for top threads

--- static/format_local_time.js
+++ static/format_local_time.js
@@ -17,4 +17,31 @@ function formatLocalTime() {
     });
 }
 
+
+function formatMTime() {
+    function timeAgo(ts) {
+	const seconds = Math.floor((Date.now() - ts) / 1000);
+
+	if (seconds < 60) return `${seconds}s`;
+	const minutes = Math.floor(seconds / 60);
+	if (minutes < 60) return `${minutes}m`;
+
+	const hours = Math.floor(minutes / 60);
+	if (hours < 24) return `${hours}h`;
+
+	const days = Math.floor(hours / 24);
+	return `${days}d`;
+    }
+
+
+    const times = document.querySelectorAll('.thread-mtime');
+
+    times.forEach(element => {
+	const utcDate = new Date(element.title.trim());
+	element.textContent = `(${timeAgo(utcDate)} ago)`;
+    });
+}
+
 formatLocalTime();
+formatMTime();
+setInterval(formatMTime, 1000);
